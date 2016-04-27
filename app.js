04a0845936b9ce8c93a540084a3df87437ddf9db
ohhheyyyy <ashley.hartt@gmail.com>
2016-04-27 17:39:20 -0400

    Complete Express configuration and begin writing app funcitonality

--- app.js
+++ app.js
@@ -6,9 +6,37 @@ var express = require( 'express' );
 // create an instance of an express application
 var app = express(); 
 
+app.use(function (req, res, next) {
+  // fs.appendFile(__dirname + '/iplog.txt', req.ip + '\n', function(err) {
+  //    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+  //       next();
+  //   });
+  //   // do your logging here
+    console.log("the request url is: ", req.url);
+    console.log("this is the verb: " , req.method);
+    next();
+   
+});
+
+app.use('/special', function (req, res, next) {
+   
+    res.send("You've reached a special area!");
+  
+    next();
+   
+});
+
 app.get('/', function(req, res){
   res.send('Hello World!');
 });
+
+app.get('/news', function(req, res){
+  res.send('Breaking news: ');
+});
+
+// app.get('/special', function(req, res){
+//   res.send("You've reached a special area!");
+// });
 
 app.listen(3000, function() {
     console.log('Server listening on port 3000');
