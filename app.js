0d15a43a87acf4c68d2dc07dab711518f3b8b0f3
ohhheyyyy <ashley.hartt@gmail.com>
2016-04-28 11:56:09 -0400

    Display Twitter user profile page and tweets on homepage

--- app.js
+++ app.js
@@ -1,75 +1,33 @@
-var http = require('http');
-var fs = require('fs');
-var swig = require('swig');
-
+// REQUIREMENTS
 var express = require( 'express' );
-
-// create an instance of an express application
 var app = express(); 
+var swig = require('swig');
+// change to true in production code
+swig.setDefaults({ cache: false });
+var router = express.Router();
+var routes = require('./routes/');
+var tweetBank = require('./tweetBank');
+
+
+
+// MIDDLEWARE
 
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-// change to true in production code
-swig.setDefaults({ cache: false });
-
-var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
-
-var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+app.use(express.static(__dirname + '/public'));
 
 
-app.use('/views', function(req, res, next){
-  res.render( 'index', {title: 'Hall of Fame', people: people}, function(err, html){
-    res.send(html);
-  });
-});
-
-swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
-    console.log(output);
-
-
-});
-
-app.use(function (req, res, next) {
-  // fs.appendFile(__dirname + '/iplog.txt', req.ip + '\n', function(err) {
-  //    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
-  //       next();
-  //   });
-  //   // do your logging here
-    console.log("the request url is: ", req.url);
-    console.log("this is the verb: " , req.method);
-    next();
-   
-});
-
-app.use('/special', function (req, res, next) {
-   
-    res.send("You've reached a special area!");
-  
-    next();
-   
-});
+// ROUTES
 
 app.get('/', function(req, res){
-  res.send('Hello World!');
+  var tweets = tweetBank.list();
+  res.render("index", {title: "Twitter.js", tweets: tweets});
 });
-
-app.get('/news', function(req, res){
-  res.send('Breaking news: ');
-});
-
-// app.get('/special', function(req, res){
-//   res.send("You've reached a special area!");
-// });
 
 app.listen(3000, function() {
     console.log('Server listening on port 3000');
 });
+
+// EXPORTS, IF ANY
